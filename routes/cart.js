523d1915669724f94fc4f6b4af15c90404854f54
Alexandre Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2025-04-16 17:06:38 +0200

    Added the booking routes POST

--- routes/cart.js
+++ routes/cart.js
@@ -18,4 +18,24 @@ router.delete("/:id", (req, res) => {
   });
 });
 
+const Booking = require('../models/bookings')
+
+router.post('/', (req, res) => {
+  let {trips, hours, price} = req.body;
+
+  trips = trips.split(" ")
+  let departure = trips[0]
+  let arrival = trips[2]
+
+  let purchaseTrip = new Booking ({
+      departure: departure,
+      arrival: arrival,
+      hours: hours,
+      price: price,
+  })
+  purchaseTrip.save()
+  .then(res.json({purchaseTrip}))
+
+});
+
 module.exports = router;
