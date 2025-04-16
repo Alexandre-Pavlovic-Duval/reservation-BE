608b38de86a5e36485deeb3cf725076d2af2238d
Ethan <ethan.frappie@gmail.com>
2025-04-16 14:02:24 +0200

    delete cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,18 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 require("../models/connection");
-const Cart = require('../models/cart');
+const Cart = require("../models/cart");
 
-router.get('/', function(req, res) {
-    Cart.find().then(data => {
-		console.log(data)
-		res.json({ cart:data });
-	});
-}); 
+router.get("/", function (req, res) {
+  Cart.find().then((data) => {
+    console.log(data);
+    res.json({ cart: data });
+  });
+});
 
-
-
-
+router.delete("/:id", (req, res) => {
+  Cart.deleteOne({ _id: req.params.id }).then(() => {
+    Cart.find().then((data) => {
+      res.json({ result: true, cart: data });
+    });
+  });
+});
 
 module.exports = router;
-
