7b1a12f8432359778926e57fe6b305ec220ee1e3
Alexandre Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2025-04-16 17:11:11 +0200

    added the routes cart

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var cartRouter = require('./routes/cart')
 var usersRouter = require('./routes/users');
 var tripsRouter = require('./routes/trips');
-var bookingsRouter = require('./routes/trips');
+var bookingsRouter = require('./routes/bookings');
 
 var app = express();
 const cors = require("cors");
@@ -26,6 +26,6 @@ app.use('/', indexRouter);
 app.use('/cart', cartRouter);
 app.use('/users', usersRouter);
 app.use('/trips', tripsRouter);
-app.use('.bookings', bookingsRouter);
+app.use('/bookings', bookingsRouter);
 
 module.exports = app;
